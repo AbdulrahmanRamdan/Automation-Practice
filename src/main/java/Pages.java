0412786c6e4508544876cc6303d6e22d52b15dc4
AbdulrahmanRamdan <90057925+AbdulrahmanRamdan@users.noreply.github.com>
2022-08-24 10:35:43 +0200

    Update Pages file to support google search page

--- src/main/java/Pages.java
+++ src/main/java/Pages.java
@@ -6,6 +6,11 @@ public class Pages {
         this.driver=driver;
     }
     public Search_Page get_Search_Page(){
+        driver.get("https://codility-frontend-prod.s3.amazonaws.com/media/task_static/qa_search/6f03f4361b080eeb747193aadd94cd2b/static/attachments/reference_page.html");
         return new Search_Page(this.driver);
+    }
+    public Google_Search get_Google_Page(){
+        driver.get("https://www.google.com/");
+        return new Google_Search(driver);
     }
 }
