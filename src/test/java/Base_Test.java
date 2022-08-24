4b500610ddc252d4272fa7c19bff8cf488633f96
AbdulrahmanRamdan <90057925+AbdulrahmanRamdan@users.noreply.github.com>
2022-08-24 10:38:33 +0200

    Add files via upload

--- src/test/java/Base_Test.java
+++ src/test/java/Base_Test.java
@@ -13,8 +13,6 @@ public class Base_Test {
     @BeforeSuite
     void setup(){
         driver= WebDriverManager.chromedriver().create();
-        driver.get("https://codility-frontend-prod.s3.amazonaws.com/media/task_static/qa_search/6f03f4361b080eeb747193aadd94cd2b/static/attachments/reference_page.html");
-        Assert.assertEquals(driver.getTitle(),"ARR");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         pages=new Pages(getDriver());
     }
