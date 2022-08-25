598802df5adc34d36d1f9c2a02c18be12f03d18a
AbdulrahmanRamdan <90057925+AbdulrahmanRamdan@users.noreply.github.com>
2022-08-25 13:44:59 +0200

    Edit classes to be able to changes

--- src/main/java/Google_Search.java
+++ src/main/java/Google_Search.java
@@ -10,11 +10,15 @@ public class Google_Search extends Element_Object{
     WebElement search_Input;
     WebElement article_Result;
     WebElement google_Logo;
+    By search_Input_Path=By.xpath("//input[@title=\"بحث\"]");
+    By search_Button_Path=By.xpath("//input[@class=\"gNO89b\"]");
+    By google_Logo_Path=By.className("lnXdpd");
+    By article_Result_Path=By.xpath("//span[contains(text(),\"What is test automation?\")]");
     public Google_Search(WebDriver webDriver){
         this.driver=webDriver;
     }
     public void set_Search_Text(String text){
-        search_Input=driver.findElement(By.xpath("//input[@title=\"بحث\"]"));
+        search_Input=driver.findElement(search_Input_Path);
         clear(search_Input);
         sendKey(search_Input,text);
     }
@@ -22,18 +26,18 @@ public class Google_Search extends Element_Object{
         driver.navigate().back();
     }
     public void click_Enter_Key(){
-        driver.findElement(By.xpath("//input[@title=\"بحث\"]")).sendKeys(Keys.ENTER);
+        driver.findElement(search_Input_Path).sendKeys(Keys.ENTER);
     }
     public void click_Search_Button(){
-        search_Button=driver.findElement(By.xpath("//input[@class=\"gNO89b\"]"));
+        search_Button=driver.findElement(search_Button_Path);
         click(search_Button);
     }
     public boolean find_Google_Logo(){
-         google_Logo=driver.findElement(By.className("lnXdpd"));
+         google_Logo=driver.findElement(google_Logo_Path);
          return google_Logo.isDisplayed();
     }
     public boolean find_Result_Article(){
-        article_Result=driver.findElement(By.xpath("//span[contains(text(),\"What is test automation?\")]"));
+        article_Result=driver.findElement(article_Result_Path);
         return article_Result.isDisplayed();
     }
 
--- src/main/java/Search_Page.java
+++ src/main/java/Search_Page.java
@@ -11,32 +11,37 @@ public class Search_Page extends Element_Object{
     WebElement empty_Message_Error;
     WebElement noResult_Message_Error;
     List<WebElement> results_Elments;
+    By input_Text_Path=By.id("search-input");
+    By search_Button_Path=By.id("search-button");
+    By empty_Message_Error_Path=By.id("error-empty-query");
+    By noResult_Message_Error_Path=By.id("error-no-results");
+    By results_Elments_Path=By.xpath("//Ul/Li");
     public Search_Page(WebDriver webdriver){
         this.driver=webdriver;}
 
     public void click_Search(){
-        search_Button=driver.findElement(By.id("search-button"));
+        search_Button=driver.findElement(search_Button_Path);
         click(search_Button);
     }
     public void clear_Search_Text(){
-        input_Text=driver.findElement(By.id("search-input"));
+        input_Text=driver.findElement(input_Text_Path);
         clear(input_Text);
     }
     public void set_Search_Text(String text){
-        input_Text=driver.findElement(By.id("search-input"));
+        input_Text=driver.findElement(input_Text_Path);
         clear(input_Text);
         sendKey(input_Text,text);
     }
     public String get_NoResult_Message(){
-        noResult_Message_Error=driver.findElement(By.id("error-no-results"));
+        noResult_Message_Error=driver.findElement(noResult_Message_Error_Path);
         return get_Text(noResult_Message_Error);
     }
     public String get_Empty_Message(){
-        empty_Message_Error=driver.findElement(By.id("error-empty-query"));
+        empty_Message_Error=driver.findElement(empty_Message_Error_Path);
         return get_Text(empty_Message_Error);
     }
     public List<String>get_Text_Results(){
-        results_Elments=driver.findElements(By.xpath("//Ul/Li"));
+        results_Elments=driver.findElements(results_Elments_Path);
         List<String>text_Results=new ArrayList<>();
         for(int i=0;i<results_Elments.size();i++){
             text_Results.add(get_Text(results_Elments.get(i)));
